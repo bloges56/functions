c1c034dfb8de960ea265a448be71cee937d4d789
Brady Logan <brady.drew.logan@gmail.com>
2020-08-26 14:33:59 -0500

    completed first three functions

---
+++ practice.js
@@ -0,0 +1,38 @@
+//Given a person's age, returns their age in dog years
+const calculateAgeInDogYears = (ageOfPerson) => {
+    const ageInDogYears = ageOfPerson / 7;
+
+    return ageInDogYears;
+}
+
+const dogAge = calculateAgeInDogYears(45);
+// console.log(dogAge);
+
+
+
+//Given a breed, returns a string correspoing to the input
+const favoriteBreed = (breed) => {
+    if(breed === "meow"){
+        return "I like cats";
+    }
+
+    return `my favorite dog breed is ${breed}`;
+}
+
+const myFavorite = favoriteBreed("shnauzer");
+
+// console.log("When it comes to pets,", myFavorite);
+
+
+
+//returns the sum of 3 numbers
+const add = (num1, num2, num3) => {
+    return num1 + num2 + num3;
+}
+
+var result = add(17,4,11);
+
+// console.log(result);
+
+
+
